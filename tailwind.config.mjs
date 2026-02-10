21672e8559deed7be0e42165d95e805e131b23fc
Nando Rifky <mr.winando@gmail.com>
2026-02-10 15:19:23 +0700

    asd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -23,5 +24,7 @@ export default {
 			}
 		},
 	},
-	plugins: [],
+plugins: [
+		typography(),
+	],
 }
